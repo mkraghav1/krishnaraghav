013e4ef83f9c1424e2b0db268e2f47043ee73433
Krishna Raghav <raghavsmart001@gmail.com>
2023-04-17 17:55:53 +0530

    Dummy project created by raghav mukkamala to release  branch

---
+++ .idea/add.java
@@ -0,0 +1,10 @@
+package hello;
+public class Add {
+    public void AddTwoNumbers {
+        int a=4
+         int b=6
+         int sum=a+b;
+         system.out println("output of two numbers is")
+
+    }
+}
